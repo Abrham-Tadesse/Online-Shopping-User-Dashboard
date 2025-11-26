873b38d27b834d526116133ce6696e55c0ac054a
Abrham-Tadesse <atadesse976@gmail.com>
2025-11-26 23:07:25 +0300

    summerze the product quantity and price

--- src/cartDisplay.tsx
+++ src/cartDisplay.tsx
@@ -1,4 +1,4 @@
-import React, {useState, type Dispatch} from "react";
+import React, {useEffect, useState, type Dispatch} from "react";
 import { totalItem, totalPrice } from "./productDisplay";
 import "./cartDisplay.css"
 
@@ -8,10 +8,10 @@ export function CartDisplay(){
      
   return(
     <>
-    <Navigation />
+    <Navigation amounts = {amount} onAmount = {setAmount}/>
     <DisplayAddeddProducts amounts = {amount} onAmount = {setAmount} />
 
-    <Footer />
+    <Footer amounts = {amount} onAmount = {setAmount}/>
     </>
   )
 }
@@ -26,10 +26,13 @@ interface addeditems{
     quantity : number,
 }
 
-const addedItemArray : addeditems[] = [{id : 1 ,name : "Watch 1" ,image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStxNO_7qy6ZbEqOkdC1_66BnhAPKK7KTDutQ&s",price : 20 , quantity : 0 }, {id : 2 , name : "shirt", image : "https://media.istockphoto.com/id/488160041/photo/mens-shirt.jpg?s=612x612&w=0&k=20&c=xVZjKAUJecIpYc_fKRz_EB8HuRmXCOOPOtZ-ST6eFvQ=", price : 50 , quantity : 0},{id : 3 ,name : "watch2" , image : "https://m.media-amazon.com/images/I/61n0aVXta7L._AC_UY1000_.jpg" , price : 40 ,quantity : 0}];
+const addedItemArray : addeditems[] = [{id : 1 ,name : "Watch 1" ,image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStxNO_7qy6ZbEqOkdC1_66BnhAPKK7KTDutQ&s",price : 20 , quantity :1}, {id : 2 , name : "shirt", image : "https://media.istockphoto.com/id/488160041/photo/mens-shirt.jpg?s=612x612&w=0&k=20&c=xVZjKAUJecIpYc_fKRz_EB8HuRmXCOOPOtZ-ST6eFvQ=", price : 50 , quantity :1},{id : 3 ,name : "watch2" , image : "https://m.media-amazon.com/images/I/61n0aVXta7L._AC_UY1000_.jpg" , price : 40 ,quantity :1}];
 
 
-export function Navigation(){
+export function Navigation({amounts,onAmount} : {amounts : addeditems[], onAmount : Dispatch<React.SetStateAction<addeditems[]>>}){
+
+    const totalPrice = amounts.reduce((tot,obj) => tot+(obj.price*obj.quantity),0);
+    const totalItems = amounts.reduce((items,objItem) => items + (objItem.quantity),0);
 
   return(
 
@@ -39,7 +42,7 @@ export function Navigation(){
     <h2> Acme co.</h2>
       </section>
       <section className="price-num">
-        <p>total item <span className="value"> {`${totalItem}`}</span></p>
+        <p>total item <span className="value"> {`${totalItems}`}</span></p>
         <p>total price <span className="value">{`$ ${totalPrice}`}</span></p>
         <p><button className="view">view product</button></p>
       </section>
@@ -50,12 +53,16 @@ export function Navigation(){
 }
 
 
-export function Footer(){
+export function Footer({amounts,onAmount} : {amounts : addeditems[], onAmount : Dispatch<React.SetStateAction<addeditems[]>>}){
+
+    const totalPrice = amounts.reduce((tot,obj) => tot+(obj.price*obj.quantity),0);
+    const totalItems = amounts.reduce((items,objItem) => items + (objItem.quantity),0);
+
 
   return(
     <>
       <section className="footer price-num">
-        <p>total item <span className="value"> {`${totalItem}`}</span></p>
+        <p>total item <span className="value"> {`${totalItems}`}</span></p>
         <p>total price <span className="value">{`$ ${totalPrice}`}</span></p>
         <button>place order</button>
       </section>
@@ -70,30 +77,33 @@ export function DisplayAddeddProducts({amounts,onAmount} : {amounts : addeditems
 
 
   const handleSelect = function(id : number , quantity : number){
-      onAmount(prev => amounts.map(pr => (pr.id === id ? {...pr,quantity} : pr))); // Check the id of the product and if it is correct replace the quantity
+      onAmount(prev => prev.map(pr => (pr.id === id ? {...pr,quantity} : pr))); // Check the id of the product and if it is correct replace the quantity
       
+  } 
+
+  const handleButton = function(id: number){
+       onAmount(prev => prev.filter(pr => pr.id!==id));
   }
-  
+
+
 
   return(
     <>
       
-      {addedItemArray.map( item => (
+      {amounts.map( item => (
          <li key={item.id}> <img src={item.image} alt="The product picture is not found" className="image"/> <span>{item.name}</span> <span>{item.price}</span> 
 
-         <select title="camount" className="options" onChange={ (e : React.ChangeEvent<HTMLSelectElement>) => handleSelect(item.id , Number((e.target.value)))}>
+         <select value={item.quantity} title="camount" className="options" onChange={(e : React.ChangeEvent<HTMLSelectElement>) => handleSelect(item.id , Number((e.target.value)))}>
               {Array.from({ length: 15 }, (_, i) => (
               <option key={i + 1} value={i+1}>
                    {i + 1}
               </option>
              ))}
-        </select>{amounts.map(element => (<span>{element.price * element.quantity}</span>))}
-         <button className="remove"> &times; </button></li>
+        </select> 
+        <span>{item.price * item.quantity}</span>
+         <button className="remove" onClick={() => handleButton(item.id)}> &times; </button></li>
       ))}
     </>
   )
 }
 
-// amounts.forEach(element => {
-//             (element.id === id ? element.price*element.quantity : element.price);
-//       })
