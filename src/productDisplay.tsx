17c836ae9804bd04a38102c2036ffea1ff23cb3f
Abrham-Tadesse <atadesse976@gmail.com>
2025-11-28 15:10:25 +0300

    replace the search with Effet

--- src/productDisplay.tsx
+++ src/productDisplay.tsx
@@ -15,7 +15,7 @@ export interface productType{
   }
   export let totalPrice : number = 0 ;
   export let totalItem : number = 0;
-  let searchResult : productType[];
+  let filtered : productType[];
 
 const productArray : productType[] = [{id : 1 ,title : "Watch 1" ,image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStxNO_7qy6ZbEqOkdC1_66BnhAPKK7KTDutQ&s",price : 20 }, {id : 2 , title : "shirt", image : "https://media.istockphoto.com/id/488160041/photo/mens-shirt.jpg?s=612x612&w=0&k=20&c=xVZjKAUJecIpYc_fKRz_EB8HuRmXCOOPOtZ-ST6eFvQ=", price : 50},{id : 3 ,title : "watch2" , image : "https://m.media-amazon.com/images/I/61n0aVXta7L._AC_UY1000_.jpg" , price : 40 }];
 
@@ -26,8 +26,9 @@ export function DisplayProduct(){
      const [isAdded, setIsAdded] = useState<number>(0);
      const [products,setproducts] = useState<productType[]>([]);
      const [query,setQuery] = useState<string>("");
+     const [searchResult,setSearchResult] = useState<productType[]>([]);
 
-     
+   // Displaying the product when the app is intially rendered  
   useEffect(function(){
     async function effected(){
       const resp = await fetch("https://fakestoreapi.com/products");
@@ -39,24 +40,25 @@ export function DisplayProduct(){
   },[])
 
 
+  useEffect(() => {
+  const filtered = products.filter(pro =>
+    pro.title.toLowerCase().includes(query.trim().toLowerCase()));
+
+  setSearchResult(filtered);
+}, [query, products]);
+
+
   return(
     <>
-     <Navigation  query = {query} setQuery={setQuery} products = {products} setProducts = {setproducts}/>
+     <Navigation setQuery={setQuery}/>
      <Products isAdded = {isAdded} onAdded = {setIsAdded} 
-         products = {products}/>
+         products = {products} searchResult={searchResult}/>
         <Footer />
     </>
   )
 }
 
-export function Navigation({query,setQuery,products,setProducts} : {query : string,products : productType[], setProducts :Dispatch<React.SetStateAction<productType[]>> ,setQuery : Dispatch<React.SetStateAction<string>>}){
-
-          const search = function(result : string){
-            setQuery(result);
-            searchResult = products.filter(pro => pro.title.trim().toLowerCase().includes(query.toLowerCase()));
-          console.log(result);
-               }
-
+export function Navigation({setQuery} : {setQuery : Dispatch<React.SetStateAction<string>>}){
              
 
   return(
@@ -66,7 +68,7 @@ export function Navigation({query,setQuery,products,setProducts} : {query : stri
       <section className="title">
     <h2> Acme co.</h2>
       </section>
-       <input type="text" className="searchInput" placeholder="search here" onChange={(e) => search(e.target.value)}/> 
+       <input type="text" className="searchInput" placeholder="search here" onChange={(e) => setQuery(e.target.value)}/> 
       <section className="price-num">
         <p>total item <span className="value"> {`${totalItem}`}</span></p>
         <p>total price <span className="value">{`$ ${totalPrice}.00`}</span></p>
@@ -91,12 +93,12 @@ export function Footer(){
   )
 }
 
-export function Products({isAdded,onAdded,products} :
-   {isAdded : number,products : productType[],onAdded : React.Dispatch<React.SetStateAction<number>>}){
+export function Products({isAdded,onAdded,products,searchResult} :
+   {isAdded : number,products : productType[],searchResult:productType[],onAdded : React.Dispatch<React.SetStateAction<number>>}){
 
   const [addedCart , setAddedCart] = useState<number[]>([]);
   const displayAllProduct : productType[] = searchResult? searchResult : products;
-
+// handling the add to cart button
     function handleCart(item : productType){
        onAdded(prev => prev + 1);
        totalPrice += item.price;
